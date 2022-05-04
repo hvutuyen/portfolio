b0084ad4c10f9b6da5f92089aec46053aff59fb3
Hoang Vu Tuyen <hoang.vutuyen@live.de>
2022-05-04 02:03:46 +0200

    add media settings

--- finance_app/settings.py
+++ finance_app/settings.py
@@ -129,6 +129,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static", # basedir static folder
 ]
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
